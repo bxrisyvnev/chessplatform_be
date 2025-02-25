b6da04a18e7849a2a9851c0c8dbf5ebc3f522ed7
Yanev, Boris <borisyanev0000@gmail.com>
2025-02-25 14:13:33 +0100

    upload project to git

---
+++ BackEnd/S3-ChessPlatform/src/main/java/org/example/s3chessplatform/domain/User.java
@@ -0,0 +1,27 @@
+package org.example.s3chessplatform.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private int age;
+    private String displayName;
+    private String nationality;
+
+    public User(String username, String password, int age, String displayName, String nationality) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.displayName = displayName;
+        this.nationality = nationality;
+    }
+}
---
+++ BackEnd/S3-ChessPlatform/src/test/java/org/example/s3chessplatform/S3ChessPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.s3chessplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ChessPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
