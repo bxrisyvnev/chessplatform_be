7dc62e25c7cc3cdcdb98cc29d7e80f5bee893638
Yanev, Boris <borisyanev0000@gmail.com>
2025-02-28 02:36:13 +0100

    some minor changes

---
+++ ChessplatformBE/src/main/java/org/example/chessplatformbe/ChessplatformBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.chessplatformbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessplatformBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessplatformBeApplication.class, args);
+    }
+
+}
---
+++ ChessplatformBE/src/main/java/org/example/chessplatformbe/business/UserService.java
@@ -0,0 +1,50 @@
+package org.example.chessplatformbe.business;
+
+import org.example.chessplatformbe.domain.User;
+import org.example.chessplatformbe.persistence.IUserRepository;
+import org.example.chessplatformbe.persistence.MockUserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements IUserService {
+    private final IUserRepository userRepository;
+
+    public UserService(MockUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> updateUser(Long id, User updatedUser) {
+        return userRepository.findById(id).map(existingUser -> {
+            existingUser.setUsername(updatedUser.getUsername());
+            existingUser.setPassword(updatedUser.getPassword());
+            existingUser.setAge(updatedUser.getAge());
+            existingUser.setDisplayName(updatedUser.getDisplayName());
+            existingUser.setNationality(updatedUser.getNationality());
+            return userRepository.save(existingUser);
+        });
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
