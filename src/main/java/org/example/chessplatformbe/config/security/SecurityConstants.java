0868508179202342f252339df8f505659bda9743
Yanev, Boris <borisyanev0000@gmail.com>
2025-06-13 02:35:01 +0200

    fixed ports

--- src/main/java/org/example/chessplatformbe/config/security/SecurityConstants.java
+++ src/main/java/org/example/chessplatformbe/config/security/SecurityConstants.java
@@ -11,4 +11,5 @@ public final class SecurityConstants {
     public static final String STREAM_ENDPOINT = "/streams";
     public static final String SPECTATE_ENDPOINT = "/spectate/**";
     public static final String NEWS_ENDPOINT = "/news";
+    public static final String CHESS_ENDPOINT = "/chess";
 }
